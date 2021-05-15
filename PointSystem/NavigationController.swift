a00e916a2617e9da52f993e8e1b62afee7a80114
AaivSyMemanh <yingtianyo@163.com>
2021-05-15 18:04:41 +0800

    20171010

---
+++ PointSystem/NavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationController.swift
+//  PointSystem
+//
+//  Created by jpg on 2017/10/10.
+//  Copyright © 2017年 dongruixuan. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class NavigationController: UINavigationController{
+}
